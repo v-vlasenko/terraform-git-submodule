d228ec562ccf110945387e17ee14fea86aa8c30c
Vlad Vlasenko <vladvlasenko@Vlads-MacBook-Air.local>
2023-03-09 17:18:02 +0200

    add things

---
+++ submodule/main.tf
@@ -0,0 +1,25 @@
+resource "random_pet" "example" {
+  keepers = {
+    string  = var.string
+  }
+}
+
+variable "string" {
+  default   = "qwerty"
+  type = string
+}
+
+resource "random_pet" "top_level" {
+  keepers = {
+    # Generate a new pet name each time
+    time = timestamp()
+  } 
+}
+
+output "top_level_pet" {
+  value = resource.random_pet.top_level.id
+}
+
+module "submodule" {
+  source = "./terraform-scalr-flat"
+}
